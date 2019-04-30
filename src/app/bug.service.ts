f77e8d8f741d0ec7d384989dd594f0bec81f6a03
4oker <39918916+4oker@users.noreply.github.com>
2019-04-30 18:50:54 +0200

    rrrr

--- src/app/bug.service.ts
+++ src/app/bug.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Router} from '@angular/router'
-import { Observable} from 'rxjs';
-import { Product,CartProduct} from '../skelet';
+import { Router } from '@angular/router'
+import { Observable } from 'rxjs';
+import { Product, CartProduct } from '../skelet';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -11,89 +11,59 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class BugService {
-  
-   baseUrl= 'http://localhost:3000';
-   cartproducts:CartProduct[]=[];
-   total:number=0;
-   products:Product[]=[];
-   status:boolean=true;
-   
-  constructor(
-    private http: HttpClient) { }
-    private router:Router
 
+  baseUrl = 'http://localhost:3000';
+  cartproducts: CartProduct[] = [];
+  total: number = 0;
+  products: Product[] = [];
+  status: boolean = true;
+
+  constructor(private http: HttpClient) { }
     
-
-
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl+'/products');
+    return this.http.get<Product[]>(this.baseUrl + '/products');
   }
 
-  
-
-  getProduct(id:number): Observable<Product> {
+  getProduct(id: number): Observable<Product> {
     return this.http.get<Product>(this.baseUrl + '/products/' + id)
-    
-  }  
-  addProduct(product:CartProduct): void {
+  }
 
+  addProduct(product: CartProduct): void {
     this.http.get<CartProduct>(this.baseUrl + '/cartProducts/' + product.id).subscribe(
       dbCart => {
         dbCart.count += 1;
-        dbCart.name=product.name
-        dbCart.price=product.price
-        dbCart.image=product.image
-        dbCart.id=product.id
-      
-     
-      
+        dbCart.name = product.name
+        dbCart.price = product.price
+        dbCart.image = product.image
+        dbCart.id = product.id
+
         this.http.put(this.baseUrl + '/cartProducts/' + dbCart.id, dbCart)
-          .subscribe(res => {
-            
-          });   
-             },
+          .subscribe(res => {});
+      },
       error => {
-        
-        
-
         this.http.post(this.baseUrl + '/cartProducts', product)
-        .subscribe(res => {res}
-          );
-        
-  }
-  );
+          .subscribe(res => { res });
+      }
+    );
   }
 
-
-  
-
-
-  removePr(id:number){
-   
-     return this.http.delete<CartProduct>(this.baseUrl+'/cartProducts/'+id)
-    
+  removePr(id: number) {
+    return this.http.delete<CartProduct>(this.baseUrl + '/cartProducts/' + id)
   }
 
   minusProduct(CartProduct: CartProduct): Observable<CartProduct> {
-    CartProduct.count = CartProduct.count -1
-    CartProduct.price = CartProduct.price 
-    return this.http.put<CartProduct>(this.baseUrl+'/cartProducts/'+CartProduct.id, CartProduct)
+    CartProduct.count = CartProduct.count - 1
+    CartProduct.price = CartProduct.price
+    return this.http.put<CartProduct>(this.baseUrl + '/cartProducts/' + CartProduct.id, CartProduct)
   }
 
-  
   plusProduct(CartProduct: CartProduct): Observable<CartProduct> {
-    CartProduct.count = CartProduct.count +1
-    CartProduct.price = CartProduct.price 
-    
-    
-    return this.http.put<CartProduct>(this.baseUrl+'/cartProducts/'+CartProduct.id, CartProduct)
+    CartProduct.count = CartProduct.count + 1
+    CartProduct.price = CartProduct.price
+    return this.http.put<CartProduct>(this.baseUrl + '/cartProducts/' + CartProduct.id, CartProduct)
   }
- 
-  getCartProducts():Observable<CartProduct[]>{
-    return this.http.get<CartProduct[]>(this.baseUrl+'/cartProducts')
-    
+
+  getCartProducts(): Observable<CartProduct[]> {
+    return this.http.get<CartProduct[]>(this.baseUrl + '/cartProducts')
   }
- 
-  
-  
 }
